923b4e056522085c0d6238a1172a9e9ef913cb01
SanGit56 <radhiyanrmhhisan@gmail.com>
2024-01-26 15:14:27 +0700

    mengubah struktur tabel users membuat file migration untuk 6 tabel baru

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('username', 16);
+            $table->string('name', 64);
+            $table->string('email', 64)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 256);
+            $table->boolean('status');
+            $table->string('profile_picture', 32)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_01_26_071204_create_submenus_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('submenus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('name', 32);
+            $table->string('position', 4);
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('submenus');
+    }
+};
