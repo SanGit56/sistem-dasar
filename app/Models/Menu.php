4a0b550cd1ce640a817071d054d378213eb83d0c
SanGit56 <radhiyanrmhhisan@gmail.com>
2024-01-26 17:47:50 +0700

    menambahkan 3 model, 3 controller, dan 1 view baru

---
+++ app/Models/Menu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+
+    // protected $table = 'menus';      opsional. perlu jika nama tabel tidak sama atau bukan bentuk jamak dari nama model
+}
